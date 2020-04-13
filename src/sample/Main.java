e2fc6b4e73ed64bdfb43bc1ccf7be9c0fb2155bc
MGerckens <63205374+MGerckens@users.noreply.github.com>
2020-04-13 15:51:19 -0400

    Testing updates

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,16 +1,30 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.FileInputStream;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        VBox root = new VBox();
+        FileInputStream imgLoc = new FileInputStream("src/sample/lung healthbar1.png");
+        Image lungHealth = new Image(imgLoc);
+        ImageView imageView = new ImageView(lungHealth);
+        Label prompt = new Label();
+        Button choice1 = new Button();
+        Button choice2 = new Button();
+        Button choice3 = new Button();
+
+        root.getChildren().addAll(imageView,prompt,choice1,choice2,choice3);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
